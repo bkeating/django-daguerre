86cc246d70ff7313e01ee8aadf53f0cb37fd747c
Benjamin Keating <ben@abstractly.io>
2024-09-09 13:04:24 -0500

    Replaced `url` with `path`

--- daguerre/urls.py
+++ daguerre/urls.py
@@ -1,17 +1,20 @@
-from django.urls import re_path
+from django.urls import path
 
 from daguerre.views import (AdjustedImageRedirectView, AjaxAdjustmentInfoView,
                             AjaxUpdateAreaView)
 
 
 urlpatterns = [
-    re_path(r'^adjust/(?P<storage_path>.+)$',
-        AdjustedImageRedirectView.as_view(),
-        name="daguerre_adjusted_image_redirect"),
-    re_path(r'^info/(?P<storage_path>.+)$',
-        AjaxAdjustmentInfoView.as_view(),
-        name="daguerre_ajax_adjustment_info"),
-    re_path(r'^area/(?P<storage_path>.+?)(?:/(?P<pk>\d+))?$',
-        AjaxUpdateAreaView.as_view(),
-        name="daguerre_ajax_update_area"),
+    path('adjust/<path:storage_path>',
+         AdjustedImageRedirectView.as_view(),
+         name="daguerre_adjusted_image_redirect"),
+    path('info/<path:storage_path>',
+         AjaxAdjustmentInfoView.as_view(),
+         name="daguerre_ajax_adjustment_info"),
+    path('area/<path:storage_path>/<int:pk>',
+         AjaxUpdateAreaView.as_view(),
+         name="daguerre_ajax_update_area"),
+    path('area/<path:storage_path>',
+         AjaxUpdateAreaView.as_view(),
+         name="daguerre_ajax_update_area"),
 ]
